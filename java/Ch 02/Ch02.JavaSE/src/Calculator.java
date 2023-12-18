9cb7b8db7b49d951c9d29c07bacacdcba56d09f4
newbigwater <newbigwater@gmail.com>
2023-12-18 17:48:43 +0900

    [CH02] Java SE Project

---
+++ java/Ch 02/Ch02.JavaSE/src/Calculator.java
@@ -0,0 +1,11 @@
+public class Calculator {
+    public static void main(String[] args) {
+        int firNum, secNum, result;
+
+        firNum = 1;
+        secNum = 1;
+
+        result = firNum + secNum;
+        System.out.println(result);
+    }
+}
---
+++ java/Ch 02/Ch02.JavaSE/src/FourArithmetic.java
@@ -0,0 +1,11 @@
+public class FourArithmetic {
+    public static void main(String[] args) {
+        int a = 12, b;
+        b = 2;
+
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("a / b = " + (a / b));
+    }
+}
